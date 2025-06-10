f7dad519a8a47c1a9cb557ebde125f2095e0ae4d
Anton Kutuzov <anton_kutuzov@icloud.com>
2025-06-10 09:27:27 +0200

    solved 16 amn 209 problems

--- src/main/java/leetcode/arrays/medium/FindAllDuplicatesinAnArray/Main.java
+++ src/main/java/leetcode/arrays/medium/FindAllDuplicatesinAnArray/Main.java
@@ -1,11 +1,12 @@
-package leetcode.arrays.medium.FindPeakElement;
+package leetcode.arrays.medium.FindAllDuplicatesinAnArray;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] array = {1, 2, 7, 8, 5, 3, 2, 1};
-        //int[] array = {1,2,3,5,8,7,2,1};
-        System.out.println(solution.findPeakElement((array)));
+        int[] array = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] array2 = {1, 2, 3, 5, 8, 7, 2, 1};
+        System.out.println(solution.findDuplicates((array)));
+        System.out.println(solution.findDuplicates((array2)));
     }
 }
